b18315540fb59d343da9dc54b65797290f9b6dd2
haawwkeye <connorbishop53@gmail.com>
2023-05-20 18:05:18 -0400

    Add module for keyboard, Add build.bat, etc

---
+++ buildModel.bat
@@ -0,0 +1,6 @@
+@echo off
+
+@REM Build the roblox place for testing
+rojo build -o RobloxVR.rbxmx
+
+exit
---
+++ buildPlace.bat
@@ -0,0 +1,6 @@
+@echo off
+
+@REM Build the roblox place for testing
+rojo build place.project.json -o RobloxVR.rbxlx
+
+exit
