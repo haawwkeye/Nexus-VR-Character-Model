ab7f2ac17a2c3106c491af3a30deb1ca5e05b684
haawwkeye <connorbishop53@gmail.com>
2023-05-21 08:26:54 -0400

    Add support for starting the file after building

--- buildPlace.bat
+++ buildPlace.bat
@@ -3,4 +3,13 @@
 @REM Build the roblox place for testing
 rojo build place.project.json -o RobloxVR.rbxlx
 
-exit
+:restart
+choice /c yn /n /m "Would you like to open the place (Y/N)? "
+
+if %errorlevel% == 1 (
+    start RobloxVR.rbxlx
+) else if %errorlevel% == 2 (
+    exit
+) else (
+    goto restart
+)
