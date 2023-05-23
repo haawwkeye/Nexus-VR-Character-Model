c288e5aea6ce8170cb5ee25eb7d226eaa77debdd
haawwkeye <connorbishop53@gmail.com>
2023-05-22 21:58:12 -0400

    Add an installer for jq.exe probably not the best way of doing it but it works

--- build.bat
+++ build.bat
@@ -7,6 +7,17 @@ for /f "tokens=1,2 delims=:{} " %%A in (default.project.json) do (
 
 set Name=%Name:",=%
 
+if not exist jq.exe (
+    goto DOES_NOT_EXIST
+) else (
+    goto EXISTS
+)
+
+:DOES_NOT_EXIST
+echo Installing JQ as it's missing
+Installer.bat
+exit
+:EXISTS
 @REM Create the Sourcemap.json
 @REM Downloaded jq from https://stedolan.github.io/jq/
 echo Building sourcemap.json
@@ -17,13 +28,16 @@ echo Built sourcemap.json
 echo What would you like to do?
 echo 1 - Build %Name%.rbxlx
 echo 2 - Build %Name%.rbxmx
+echo 3 - Exit
 
-choice /c 12 /n /m "Choose a option: "
+choice /c 123 /n /m "Choose a option: "
 
 if %errorlevel% == 1 (
     BuildPlace.bat
 ) else if %errorlevel% == 2 (
     BuildModel.bat
+) else if %errorlevel% == 3 (
+    exit
 ) else (
     goto restart
 )
